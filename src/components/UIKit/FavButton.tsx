5b9b576d53d99a4a399d07c8ad2100294725ed2e
XtzZZZ <maxim.savochkin120@gmail.com>
2025-02-22 16:07:55 +0100

    Change FavButton icons from heart to star
    Replaced heart icons (FaHeart, FaRegHeart) with star icons (FaStar, FaRegStar) to align with updated design requirements. This ensures consistent visual representation across the application.

--- src/components/UIKit/FavButton.tsx
+++ src/components/UIKit/FavButton.tsx
@@ -1,5 +1,5 @@
 import styles from "../../styles/components/DiseasesPage/DiseaseCard.module.scss";
-import {FaHeart, FaRegHeart} from "react-icons/fa";
+import {FaRegStar, FaStar} from "react-icons/fa";
 import {useState} from "react";
 
 export default function FavButton() {
@@ -10,9 +10,9 @@ export default function FavButton() {
     return (
         <button className={styles.fav_btn} onClick={toggleFavourite}>
             {isFavourite ? (
-                <FaHeart className={styles.fav_icon} /> // Filled heart when active
+                <FaStar className={styles.fav_icon} /> // Filled heart when active
             ) : (
-                <FaRegHeart className={styles.fav_icon} /> // Outline heart when not active
+                <FaRegStar className={styles.fav_icon} /> // Outline heart when not active
             )}
         </button>
     );
