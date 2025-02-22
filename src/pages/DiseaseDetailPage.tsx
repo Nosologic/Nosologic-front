df4831cb5506a67e1d27ce5649fc569913176afd
XtzZZZ <maxim.savochkin120@gmail.com>
2025-02-22 14:03:27 +0100

    Enhance DiseaseDetailPage with additional details table
    Added a table displaying extended disease details such as locations, alternative names, locus, and OMIM. Updated styles to support the new table and its sections, ensuring proper layout and readability.

--- src/pages/DiseaseDetailPage.tsx
+++ src/pages/DiseaseDetailPage.tsx
@@ -5,7 +5,7 @@ import diseases from "../assets/Diseases.ts";
 export default function DiseaseDetailPage() {
     const { id } = useParams<{ id: string }>();
     const numericId = Number(id ?? "0");
-    const {name, description} = diseases[numericId - 1];
+    const {name, locations, alt_names, locus, omim, description} = diseases[numericId - 1];
 
     return (
         <main className={styles.disease_detail_page_ctr}>
@@ -21,7 +21,31 @@ export default function DiseaseDetailPage() {
                         {description}
                     </span>
                 </div>
-
+                <table className={styles.disease_info_table} role={"table"}>
+                    <caption className={styles.table_caption}>{name}</caption>
+                    <tbody>
+                        <tr>
+                            <th scope={"row"} className={styles.table_label}>Locations:</th>
+                            {locations.map((loc) => (
+                                <td className={styles.table_data} key={loc}>{loc}</td>
+                            ))}
+                        </tr>
+                        <tr>
+                            <th scope={"row"} className={styles.table_label}>Alt. names:</th>
+                            {alt_names.map((an) => (
+                                <td className={styles.table_data} key={an}>{an}</td>
+                            ))}
+                        </tr>
+                        <tr>
+                            <th scope={"row"} className={styles.table_label}>Locus:</th>
+                            <td className={styles.table_data}>{locus}</td>
+                        </tr>
+                        <tr>
+                            <th scope={"row"} className={styles.table_label}>Omim:</th>
+                            <td className={styles.table_data}>{omim}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </main>
     )
