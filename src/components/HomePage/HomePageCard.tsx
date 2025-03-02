c3a959e5b328acf9487b30467a50e7cd44f2a881
XtzZZZ <maxim.savochkin120@gmail.com>
2025-03-02 13:55:42 +0100

    Add dynamic disease cards to HomePage with icons
    This update enhances the `HomePage` by dynamically rendering disease cards using data from the `Diseases` asset. Each card now includes styled icons to display the number of locations and loci when relevant. The `HomePageCard` component was refactored to support these properties, and minor styling updates were applied for improved layout.

--- src/components/HomePage/HomePageCard.tsx
+++ src/components/HomePage/HomePageCard.tsx
@@ -1,11 +1,40 @@
 import styles from "../../styles/components/HomePage/HomePageCard.module.scss";
+import {IoLocationOutline} from "react-icons/io5";
+import {SiMicrogenetics} from "react-icons/si";
 
-export default function HomePageCard() {
+interface IHomePageCardProps {
+    name: string;
+    purpose: string;
+    locations_number?: number;
+    locus_number?: number;
+    date?: string;
+}
+
+export default function HomePageCard({ name, purpose, locations_number, locus_number } : Readonly<IHomePageCardProps>) {
+
+    const defineContent = () => {
+        if (purpose === "disease") {
+            return (
+                <div className={styles.hpc_info}>
+                    <div className={styles.hpc_info_section}>
+                        <IoLocationOutline />
+                        {locations_number}
+                    </div>
+                    <div>
+                        <SiMicrogenetics />
+                        {locus_number}
+                    </div>
+                </div>
+            )
+        }
+    }
+
     return (
         <div className={styles.home_page_card}>
             <span className={styles.card_name}>
-
+                {name}
             </span>
+            {defineContent()}
         </div>
     )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,18 @@
 import styles from '../styles/pages/HomePage.module.scss';
 import HomePageCard from "../components/HomePage/HomePageCard.tsx";
+import diseases from "../assets/Diseases.ts";
 
 export default function HomePage() {
     return (
        <main className={styles.home_page_ctr}>
            <div className={styles.qa_section}>
                <span className={styles.page_subtitle}>Diseases</span>
+               {diseases
+                   .filter( (dis) => dis.visibility)
+                   .map( (dis) => (
+                   <HomePageCard key={dis.id} name={dis.name} purpose={"disease"}
+                                 locations_number={dis.locations.length} locus_number={dis.table_data.locus.length}/>
+               ))}
            </div>
            <div className={styles.qa_section}>
                <span className={styles.page_subtitle}>Clusters</span>
@@ -13,7 +20,7 @@ export default function HomePage() {
            <div className={styles.qa_section}>
                <span className={styles.page_subtitle}>Blogs</span>
                <div className={styles.qa_cards_ctr}>
-                   <HomePageCard/>
+
                </div>
            </div>
        </main>
