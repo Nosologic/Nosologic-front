87246b630ce4ea4eefaf77eea6fbc9c5bb4fc6e6
XtzZZZ <maxim.savochkin120@gmail.com>
2025-04-24 13:07:46 +0200

    Update styles and improve responsiveness in multiple components
    Refined styles for MissionPage, DiseaseCard, and Header to enhance alignment, spacing, and text visibility across different screen sizes. Improved DiseaseCard logic to handle pluralization and visibility of locations, alternative names, and loci. Introduced new utility mixins and updated global width settings for better layout consistency.

--- src/components/DiseasesPage/DiseaseCard.tsx
+++ src/components/DiseasesPage/DiseaseCard.tsx
@@ -7,6 +7,9 @@ import FavButton from "../UIKit/FavButton.tsx";
 export default function DiseaseCard({id}: Readonly<{ id: number }>) {
     const { name, locations, table_data } = diseases[id - 1];
     const { alt_names, locus } = table_data;
+    const visibleLocations = locations
+        .filter((loc) => loc.visibility);
+
     return (
         <div className={styles.disease_card}>
             <div className={styles.dc_upper_ctr}>
@@ -19,16 +22,16 @@ export default function DiseaseCard({id}: Readonly<{ id: number }>) {
             <div className={styles.disease_card_info_ctr}>
                 {/* dc stands for disease card */}
                 <div className={styles.dc_info_ctr}>
-                    <span className={styles.dc_info_title}>Locations:</span>
+                    <span className={styles.dc_info_title}>{ visibleLocations.length > 1 ? "Locations:" : "Location:" }</span>
                     <div className={styles.dc_info_overflow}>
-                        {locations.map(((loc) => (
+                        {visibleLocations.map(((loc) => (
                             <span key={loc.id} className={styles.tag}>{loc.location}</span>
                         )))}
                     </div>
                 </div>
 
                 <div className={styles.dc_info_ctr}>
-                    <span className={styles.dc_info_title}>Alternative Names:</span>
+                    <span className={styles.dc_info_title}>Alternative name{ alt_names.length > 1 ? "s" : "" }:</span>
                     <div className={styles.dc_info_overflow}>
                         {alt_names.map(((name) => (
                             <span key={name} className={styles.tag}>{name}</span>
@@ -36,7 +39,7 @@ export default function DiseaseCard({id}: Readonly<{ id: number }>) {
                     </div>
                 </div>
                 <div className={styles.dc_info_ctr}>
-                    <span className={styles.dc_info_title}>Locus:</span>
+                    <span className={styles.dc_info_title}>{ locus.length > 1 ? "Loci:" : "Locus:" }</span>
                     <div className={styles.dc_info_overflow}>
                         {locus.map(((locus) => (
                             <span key={locus} className={styles.tag}>{locus}</span>
